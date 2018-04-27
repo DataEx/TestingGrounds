8442a6a54fea5e4d68f4b373d2c22ac1d0ada409
DataEx <SethMishan@gmail.com>
2018-04-27 16:20:27 -0300

    Reorganized folder structure and renamed several C++ classes

--- TestingGrounds/Intermediate/Build/Win64/UE4Editor/Inc/TestingGrounds/TestingGrounds.init.gen.cpp
+++ TestingGrounds/Intermediate/Build/Win64/UE4Editor/Inc/TestingGrounds/TestingGrounds.init.gen.cpp
@@ -20,8 +20,8 @@ void EmptyLinkFunctionForGeneratedCodeTestingGrounds_init() {}
 			ReturnPackage = CastChecked<UPackage>(StaticFindObjectFast(UPackage::StaticClass(), nullptr, FName(TEXT("/Script/TestingGrounds")), false, false));
 			ReturnPackage->SetPackageFlags(PKG_CompiledIn | 0x00000000);
 			FGuid Guid;
-			Guid.A = 0xE4821D9C;
-			Guid.B = 0x4F97F6DB;
+			Guid.A = 0x84149681;
+			Guid.B = 0x49B646C7;
 			Guid.C = 0x00000000;
 			Guid.D = 0x00000000;
 			ReturnPackage->SetGuid(Guid);
